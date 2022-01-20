61172ef9569da327dd6598e0e9bac2f81c793605
39331 <39331@DESKTOP-J2JB5SK>
2022-01-20 11:28:53 +0100

    riprova alluna

--- src/main/java/ivrea/italia/prova1/Main.java
+++ src/main/java/ivrea/italia/prova1/Main.java
@@ -3,6 +3,6 @@ package ivrea.italia.prova1;
 public class Main {
 	public static void main(String[] args) {
         EntryPoint.main(args);
-        // prova di commit
+        // prova di commit di fattica
     }
 }
