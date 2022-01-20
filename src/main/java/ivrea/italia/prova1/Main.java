20624405d0a71bab741ecd7882d7af1726bb3e26
39331 <39331@DESKTOP-J2JB5SK>
2022-01-20 11:42:38 +0100

    ssssss

--- src/main/java/ivrea/italia/prova1/Main.java
+++ src/main/java/ivrea/italia/prova1/Main.java
@@ -3,6 +3,6 @@ package ivrea.italia.prova1;
 public class Main {
 	public static void main(String[] args) {
         EntryPoint.main(args);
-        // prova di commit di fattica 34555
+        // prova di commit di fattica 34555 7988966
     }
 }
